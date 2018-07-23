2a8013667c8393fa9ce186c17f4d796af0f9c8e7
Jason Viger <jason.viger@gmail.com>
2018-07-23 04:39:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,20 @@
 def turn_count(board)
-  
+  turn = 0
   board.each do |square|
-    
-  
+    if square == " "
+      turn += 1 
+    end
+  return turn
 end
 
 
 
-def current_player
-  
+def current_player()
+  turn = turn_count()
+  if turn % 2 == 0 
+    player = "O"
+  else player = "X"
+  end
+return player
   
 end
